c8bc5eb647a21faad239f4681ccffff057e4f1f9
RyanB156 <bressetteryan@gmail.com>
2020-03-12 13:10:03 -0400

    Implemented basic Bluetooth functionality

--- app/src/main/java/com/example/seniordesign4/HomeActivity.java
+++ app/src/main/java/com/example/seniordesign4/HomeActivity.java
@@ -1,19 +1,27 @@
 package com.example.seniordesign4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
 import android.view.View;
 import android.content.Intent;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.widget.TextView;
 import android.os.Bundle;
 import android.widget.Toast;
+import java.util.Set;
+import android.util.Log;
+
 
 public class HomeActivity extends AppCompatActivity {
 
-    private static final int REQUEST_ENABLE_BT = 0;
+    private static final int REQUEST_ENABLE_BT = 2;
     private static final int REQUEST_DISCOVER_BT = 1;
 
     TextView mStatusBlueTv, mPairedTv;
-    ImageView mBlueIv;
-    Button mOnBtn, mOffBtn, mDiscoverBtn, mPairedBtn;
 
     BluetoothAdapter mBlueAdapter;
 
@@ -22,25 +30,42 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
         mStatusBlueTv = findViewById(R.id.mStatusBlueTv);
         mPairedTv = findViewById(R.id.mPairedTv);
 
-        // TODO: Initialize the button variables...
 
-        if (mBlueAdapter == null) {
-            mStatusBueTv.setText("Bluetooth is not available");
-        } else {
-            mStatusBlueTv.setText("Bluetooth is available");
-        }
-
-        if (mBlueAdapter.isEnabled()) {
-
-        }
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
 
         // Load data passed in from the Login activity and display it in a Toast.
         Intent intent = getIntent();
         String welcome = intent.getStringExtra("welcomeMessage");
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+    }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.d("tag", "Receiving in Broadcast Receiver");
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                Log.d("tag", "Found " + deviceName + " at address " + deviceHardwareAddress);
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
+        mBlueAdapter.cancelDiscovery();
     }
 
     public void onclick(View view) {
@@ -55,11 +80,14 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void discoverclick(View view) {
-        if (!mBlueAdapter.isDiscovering()) {
-            showToast("Making Your Device Discoverable");
-            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-            startActivityForResult(intent, REQUEST_DISCOVER_BT);
-        }
+
+        mBlueAdapter.cancelDiscovery();
+        showToast("Making Your Device Discoverable");
+        //Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        //getActivity().startActivityForResult(intent, REQUEST_DISCOVER_BT);
+
+        mBlueAdapter.startDiscovery();
+
     }
 
     public void offclick(View view) {
@@ -88,6 +116,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
             case REQUEST_ENABLE_BT:
+                Log.d("tag", "resultCode = " + resultCode + " in onActivityResult");
                 if (resultCode == RESULT_OK) {
                     showToast("Bluetooth is on");
                 } else {
